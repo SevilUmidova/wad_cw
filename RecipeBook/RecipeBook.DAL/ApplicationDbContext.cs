3b6dbf72db3a7b79f9d150db893e2e8b6313aa9b
Sevil <sevil.umidova.su@gmail.com>
2024-09-06 18:11:52 +0500

    RecipeBook cw

---
+++ RecipeBook/RecipeBook.DAL/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.DAL
+{
+    public class ApplicationDbContext
+    {
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ RecipeBook/RecipeBook.DAL/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.DAL
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public int RecipeId { get; set; }
+    }
+}
